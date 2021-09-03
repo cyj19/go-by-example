97ab2e36caa0b8584690ccb4a141bee89cae36a9
chenyuji <201998@gree.com.cn>
2021-09-03 16:37:33 +0800

    循环队列保证线程安全

--- queue/circular/main.go
+++ queue/circular/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"time"
 )
 
 /*
@@ -11,17 +12,21 @@ import (
 	2. 队列判满 front == (rear+1)%max，队列剩余一个空闲单位时表示队列已满
 	3. 先进先出，入队，rear向前移动；出队，front向前移动
 	4. 队列最多只能存max-1个元素
+
+	改善为线程安全
+	思路：通过channal进行出队入队
 */
 
 const (
-	max = 5
+	max = 10000
 )
 
 // 定义循环队列
 type Circular struct {
-	queue [max]int
-	front int
-	rear  int
+	queue [max]int      // 队列
+	front int           // 出队位置
+	rear  int           // 入队位置
+	lock  chan struct{} // 锁
 }
 
 // 创建队列
@@ -30,6 +35,7 @@ func Create() *Circular {
 		queue: [max]int{},
 		front: 0,
 		rear:  0,
+		lock:  make(chan struct{}, 1),
 	}
 }
 
@@ -57,7 +63,10 @@ func (c *Circular) Add(value int) error {
 	if c.IsFull() {
 		return errors.New("the queue is full")
 	}
-
+	c.lock <- struct{}{}
+	defer func() {
+		<-c.lock
+	}()
 	c.queue[c.rear] = value
 	// rear 往前移动
 	c.rear = (c.rear + 1) % max
@@ -70,6 +79,10 @@ func (c *Circular) Remove() (int, error) {
 	if c.IsEmpty() {
 		return 0, errors.New("the queue is empty")
 	}
+	c.lock <- struct{}{}
+	defer func() {
+		<-c.lock
+	}()
 	value := c.queue[c.front]
 	// front往前移动
 	c.front = (c.front + 1) % max
@@ -87,18 +100,28 @@ func (c *Circular) Clear() {
 	c.rear = 0
 }
 
+/*
+	循环队列是否线程安全
+	cir.Size = max-1 说明线程安全
+*/
+func nonThreadSafe(cir *Circular) {
+
+	for n := 0; n < 10; n++ {
+		go func() {
+			for i := 1; i <= 1000; i++ {
+				if err := cir.Add(i); err != nil {
+					break
+				}
+			}
+
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println(cir.Size())
+}
+
 func main() {
 	cir := Create()
-	fmt.Println(cir.Add(1))
-	fmt.Println(cir.Add(2))
-	fmt.Println(cir.Add(3))
-	fmt.Println(cir.Add(4))
-	fmt.Println(cir.Add(5))
-	fmt.Println(cir.Size())
-
-	fmt.Println(cir.Remove())
-	fmt.Println(cir.Remove())
-	fmt.Println(cir.Remove())
-	fmt.Println(cir.Remove())
-	fmt.Println(cir.Remove())
+	nonThreadSafe(cir)
 }
